ec2020bf7aef7bd64187642539e91725a52f8a1f
James Bremner <j.bremner@ymail.com>
2024-04-03 11:39:18 +0100

    add error handling code

--- app.js
+++ app.js
@@ -20,6 +20,23 @@ app.get('/project/:id', (req, res) => {
     res.render('project', { projectId })
 })
 
+//handle 404 request
+app.use((req, res, next) => {
+    const err = new Error();
+    err.status = 404;
+    err.message = "Sorry this page doesn't exist.";
+    console.log(err.status, err.message);
+    res.status(404).send('<h1>404 - This page does not exist. Please check the URL</h1>');
+});
+
+//global error handler 
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status || 500);
+    res.render("error");
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
